2ee0b4c3df496ba1fe94715bf1701b980f726ab6
leenfay <96849930+leenfay@users.noreply.github.com>
2021-12-29 19:22:48 -0500

    halp

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import intro from "./components/intro/Intro";
+import Intro from "./components/intro/Intro";
+
+const App = () => {
+  return <div><Intro/></div>
+};
+
+export default App;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import "./lmfaointro.css"
+
+function Intro() {
+    return (
+        <div classname="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 classname="i-intro">Hello, My name is</h2>
+                    <h1 classname="i-name">John Burton</h1>
+                </div>
+            </div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+export default Intro
